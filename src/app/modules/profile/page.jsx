17c10dcaffa58c921af9f096df3a8758d02a3b18
shanu <ashanu2004@gmail.com>
2025-02-09 12:28:55 +0530

    fixing url

--- src/app/modules/profile/page.jsx
+++ src/app/modules/profile/page.jsx
@@ -1,53 +1,49 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import axiosInstance from "../utils/axiosinstance";
-// import logo from "../../image/—Pngtree—beautiful admin roles line vector_5259362.png";
+import adminContext from "../../../context/page";
 
 const Profile = () => {
+  const { adminDetails } = useContext(adminContext)
   const [profileData, setProfileData] = useState(null);
+  console.log(profileData);
 
   useEffect(() => {
     const fetchProfile = async () => {
-      const adminID = localStorage.getItem("adminID");
-      const memberAdminID = localStorage.getItem("MemberadminID");
-
-      if (memberAdminID) {
-        try {
-          console.log(`Fetching Member Profile: ${memberAdminID}`);
+  console.log(adminDetails.role);
+  
+      try {
+        if (adminDetails?.role) {
           const response = await axiosInstance.get(
-            `http://localhost:5000/v3/api/adminMember/AdminsMember/${memberAdminID}`
-          );
-          console.log("Member Profile Response:", response);
-          setProfileData(response.data.data);
-        } catch (error) {
-          console.error("Error fetching admin member profile:", error);
-        }
-      } else if (adminID) {
-        try {
-          console.log(`Fetching Admin Profile: ${adminID}`);
+            `v3/api/admin/profile/${adminDetails?.id}`
+          );          
+          setProfileData(response.data);
+        } 
+        else if (adminDetails?.role) {
           const response = await axiosInstance.get(
-            `http://localhost:5000/v3/api/admin/AdminsgetId/${adminID}`
+            `v3/api/adminMember/${adminDetails?.role}/${adminDetails?.id}`
           );
-          console.log("Admin Profile Response:", response);
-          setProfileData(response.data.data);
-        } catch (error) {
-          console.error("Error fetching admin profile:", error);
+          console.log(response);
+          setProfileData(response.data);
         }
+      } catch (error) {
+        console.error("Error fetching admin member profile:", error);
       }
     };
-
+  
     fetchProfile();
-  }, []);
+  }, [adminDetails?.role]);
+  
 
   return (
     <div className="h-screen dark:bg-gray-700 bg-gray-200 pt-12">
       <div className="max-w-sm mx-auto bg-white dark:bg-gray-900 rounded-lg overflow-hidden shadow-lg">
         <div className="border-b px-4 pb-6">
           <div className="text-center my-4">
-            {/* <img
+            <img
               className="h-32 w-32 rounded-full border-4 border-white dark:border-gray-800 mx-auto my-4"
-              src={logo}
+              src="/images/—Pngtree—beautiful admin roles line vector_5259362.png"
               alt="Profile"
-            /> */}
+            />
             <div className="py-2">
               {profileData ? (
                 <>
